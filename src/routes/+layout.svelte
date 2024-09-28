81594fb21e2891deb61314bf3255d26ba19c1f10
surya <balasurya811@gmail.com>
2024-09-28 20:54:08 +0530

    Adds tailwind and a basic home page.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import '../app.css';
+	import { browser } from '$app/environment';
+
+	if (browser) {
+		if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			document.documentElement.classList.add('dark');
+		}
+	}
+</script>
+
+<slot />
