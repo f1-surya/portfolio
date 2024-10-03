5651351471bdfe7927465d6e89b74339f4404826
surya <balasurya811@gmail.com>
2024-10-03 12:56:06 +0530

    Tries glob imports to load markdown instead of node:fs

--- src/routes/blogs/+page.server.ts
+++ src/routes/blogs/+page.server.ts
@@ -1,12 +1,16 @@
-import { readdirSync, readFileSync } from "node:fs";
-import matter from "gray-matter";
 import { parse } from "date-fns";
+import matter from "gray-matter";
 
 export async function load() {
-  const files = readdirSync("./blogs");
+  const path = import.meta.glob("../../../blogs/*.md", { query: "?raw" })
+  const files: string[] = [];
+  for (const dir in path) {
+    const file = (await path[dir]()).default as string;
+    files.push(file);
+  }
+
   const blogs = files.map((file) => {
-    const fileContent = readFileSync(`./blogs/${file}`);
-    const { data } = matter(fileContent);
+    const { data } = matter(file);
     return {
       slug: data.slug,
       title: data.title,
