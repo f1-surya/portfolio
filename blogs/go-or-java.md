522c80b298b201d9b4084e1b3d457a14345b89ea
surya <balasurya811@gmail.com>
2024-10-06 21:06:26 +0530

    Adds a blog about Go and Java

---
+++ blogs/go-or-java.md
@@ -0,0 +1,58 @@
+---
+title: Go or Java
+slug: go-or-java
+date: 6-10-2024
+---
+
+# Go or Java
+
+Before I begin, I want to make sure that I'm not recommending you to use either Go or Java. I'm simply going to rant about how I can't decide which one I should use.
+
+\
+2 years ago I wanted to create an app to put on my portfolio. This was during the early stages of my journey as a programmer.
+For this app's idea, I decided to create a clone of Twitter. For the tech-stack I chose **Django** for the backend, **Postgres**  for DB and **React**  for the frontend.
+I could've used the built-in template support of Django for frontend but I wanted 
+to show off my React skills. So I used Create-React-App. I didn't have any prior experience in web development, so it was confusing and tough at times.
+
+\
+I didn't have much trouble with the frontend except the CSS(I wish I heard about tailwind back then).
+And I struggled with the backend. Especially with designing the models. And it 
+being well structured and strict didn't help the noob me. But after some time I got it working(Only the basic functionalities).
+But when I look back at the code after some years, I feel so dumb and want to improve it.
+
+\
+I could just improve the existing code and be done with it, but my interests and tech-stack have been shifted a lot since then.
+Originally I wrote the frontend and the backend separately but now I want to build a full-stack application.
+And I can't choose between **Java** with **Spring-boot** and **Golang** with **Gin**.
+
+\
+Right now I'm unemployed and whichever stack I pick, will decide my stack of expertise.
+
+\
+I like Java and I know that might sound insane to some people, but I haven't worked with it much.
+I've developed a few **Android** apps with it and used it on **Leetcode**. Maybe if I used it
+for a few years, I might also hate it. But until then I like Java and I would love to work with it.
+
+\
+I also like Go. I haven't worked with it much but I've heard a lot of good things about it 
+and I find it intuitive.
+
+\
+Both of these stacks are solid choices and have equal amount of pros and cons.
+Java has a bigger ecosystem than Go and there more are job postings for Java developers.
+But lot of people know Java.
+
+\
+Go on the other hand is new compared to Java but rapidly rising in popularity.
+The lack of Objects does seem weird to me since I've only worked with Object Oriented languages.
+And it has less job opportunities. But it seems fun and has wide application.
+
+\
+I think I might pick Java. I've been going through Spring's documentations and it seems very interesting.
+Also I'm more familiar with Java than Go. I'm not sure yet but I'll let you know once I decide.
+
+\
+Maybe I should try to build the same app in both stacks.
+
+\
+Catch you in the next one.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,9 +11,7 @@
   <img src="MyPic.jpeg" alt="me" class="w-24 h-24 rounded-full" />
   <div class="flex flex-col justify-center h-24">
     <h1 class="text-3xl font-bold">Balasurya Ganesamoorthi</h1>
-    <i class="text-xl">
-      I love cars and I like to code.
-    </i>
+    <i class="text-xl"> I love cars and I like to code. </i>
     <div class="flex flex-row gap-3 justify-center items-center mt-2">
       <Link src="github.svg" url="https://github.com/f1-surya" />
       <Link src="linkedin.svg" url="https://www.linkedin.com/in/balasurya-ganesamoorthi/" />
@@ -25,27 +23,32 @@
 </div>
 <h2 class="text-xl font-bold mt-4 self-start">About Me:</h2>
 <p class="text-md text-start self-start">
-  I am a Software Developer from Tamilnadu, India. I have been working with
-  web technologies for the past 2 years. I am also a big fan of cars and I love racing <i>(mainly Formula 1)</i>.
+  I am a Software Developer from Tamilnadu, India. I have been working with web technologies for the
+  past 2 years. I am also a big fan of cars and I love racing <i>(mainly Formula 1)</i>.
 </p>
 <h2 class="text-xl font-bold mt-4 self-start">Skills:</h2>
 <p class="self-start text-start">
-  I worked with the <b>MERN</b> stack and <b>Flutter</b> at a professional level. I know Java, Python, Kotlin, Dart
-  and Typescript programming languages. I&apos;ve been daily driving <b>Linux</b>
-  based operating systems for the past 2 years. I also know <b>Docker</b> and <b>Nginx</b>. I&apos;ve deployed
-  multiple apps to <b>GCP</b> using the previously mentioned technologies.
+  I worked with the <b>MERN</b> stack and <b>Flutter</b> at a professional level. I know Java,
+  Python, Kotlin, Dart and Typescript programming languages. I&apos;ve been daily driving
+  <b>Linux</b>
+  based operating systems for the past 2 years. I also know <b>Docker</b> and <b>Nginx</b>.
+  I&apos;ve deployed multiple apps to <b>GCP</b> using the previously mentioned technologies.
 </p>
 <h2 class="text-xl font-bold mt-4 self-start">Experience:</h2>
 <p class="self-start text-start">
-  I worked as a Fullstack Developer at <a href="https://last2braincells.com" class="text-blue-400"
-                                          target="_blank">
-  Last 2 Brain Cells</a> for a year.
+  I worked as a Fullstack Developer at <a
+    href="https://last2braincells.com"
+    class="text-blue-400"
+    target="_blank"
+  >
+    Last 2 Brain Cells</a
+  > for a year.
 </p>
 <h2 class="text-xl font-bold mt-4 self-start">Interests:</h2>
 <p class="self-start text-start">
-  I love european sports cars like <i>Porsche</i>, <i>Koenigsegg</i> and <i>Lamborghini</i>. I like v10 engines and
-  manual transmissions.
-  I'm a huge fan of <i>Formula 1</i>, so if anybody brings it up, I will not stop talking about it.
+  I love european sports cars like <i>Porsche</i>, <i>Koenigsegg</i> and <i>Lamborghini</i>. I like
+  v10 engines and manual transmissions. I'm a huge fan of <i>Formula 1</i>, so if anybody brings it
+  up, I will not stop talking about it.
   <br />
   <br />
   I admire open source software and I'm a big fan of <i>Linux</i>.
